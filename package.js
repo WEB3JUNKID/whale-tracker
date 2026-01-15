3f873fdc6f77031afa13b51f8f9c7346f85e6e1a
WEB3JUNKID <victoruchenna2007@gmail.com>
2026-01-15 18:21:50 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "kaito-whale-tracker",
+  "version": "1.0.0",
+  "description": "Whale tracker for Render",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "ethers": "^6.7.1",
+    "node-fetch": "^2.6.11",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
